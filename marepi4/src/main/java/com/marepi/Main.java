7920eeb59d89ca7fc219db626f13b774734c8238
Mary Livingston <marylivingston@Marys-MacBook-Pro.local>
2024-01-02 23:45:44 -0600

    added servo classes

--- marepi4/src/main/java/com/marepi/Main.java
+++ marepi4/src/main/java/com/marepi/Main.java
@@ -1,7 +1,10 @@
 package com.marepi;
 
-public class Main {
+
+public class Main extends Component{
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Servo servo = new Servo();
+        servo.runServo();
     }
 }
--- marepi4/src/main/java/com/marepi/Servo.java
+++ marepi4/src/main/java/com/marepi/Servo.java
@@ -1,165 +1,49 @@
 package com.marepi;
 
+import java.time.Duration;
 
-// import com.pi4j.Pi4J;
-// import com.pi4j.io.gpio.digital.DigitalInput;
-// import com.pi4j.io.gpio.digital.DigitalOutput;
-// import com.pi4j.io.gpio.digital.DigitalState;
-// import com.pi4j.io.gpio.digital.PullResistance;
-// import com.pi4j.util.Console;
+import com.pi4j.Pi4J;
 
-public class Servo {
+public class Servo extends Component{
 
-// /*-
-//  * #%L
-//  * **********************************************************************
-//  * ORGANIZATION  :  Pi4J
-//  * PROJECT       :  Pi4J :: EXAMPLE  :: Sample Code
-//  * FILENAME      :  MinimalExample.java
-//  *
-//  * This file is part of the Pi4J project. More information about
-//  * this project can be found here:  https://pi4j.com/
-//  * **********************************************************************
-//  * %%
-//  * Copyright (C) 2012 - 2021 Pi4J
-//  * %%
-//  * Licensed under the Apache License, Version 2.0 (the "License");
-//  * you may not use this file except in compliance with the License.
-//  * You may obtain a copy of the License at
-//  *
-//  *      http://www.apache.org/licenses/LICENSE-2.0
-//  *
-//  * Unless required by applicable law or agreed to in writing, software
-//  * distributed under the License is distributed on an "AS IS" BASIS,
-//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  * See the License for the specific language governing permissions and
-//  * limitations under the License.
-//  * #L%
-//  */
+    public void runServo() {
 
+        var pi4j = Pi4J.newAutoContext();
 
-// /**
-//  * <p>This example fully describes the base usage of Pi4J by providing extensive comments in each step.</p>
-//  *
-//  * @author Frank Delporte (<a href="https://www.webtechie.be">https://www.webtechie.be</a>)
-//  * @version $Id: $Id
-//  */
+        final var servoMotor = new ServoMotor(pi4j, PIN.D22, 50, -90.0f, 90.0f, 2.0f, 12f);
 
+        // Demonstrate the percentage mapping on the servo
+        System.out.println("In 2 seconds, the servo motor will move to the left-most position which is 0%");
+        delay(Duration.ofSeconds(2));
+        servoMotor.setPercent(0);
 
-//     private static final int PIN_BUTTON = 24; // PIN 18 = BCM 24
-//     private static final int PIN_LED = 22; // PIN 15 = BCM 22
+        System.out.println("In another 2 seconds, the servo motor will show 100% by moving to the right-most position");
+        delay(Duration.ofSeconds(2));
+        servoMotor.setPercent(100);
 
-//     private static int pressCount = 0;
+        System.out.println("Last but not least, in 2 more seconds the servo will be centered to display 50%");
+        delay(Duration.ofSeconds(2));
+        servoMotor.setPercent(50);
+        
+        // Sweep once from left to right using the setAngle function
+        System.out.println("We will sweep once to the left in 2 seconds...");
+        delay(Duration.ofSeconds(2));
+        servoMotor.setAngle(-45);
 
-//     /**
-//      * This application blinks a led and counts the number the button is pressed. The blink speed increases with each
-//      * button press, and after 5 presses the application finishes.
-//      *
-//      * @param args an array of {@link java.lang.String} objects.
-//      * @throws java.lang.Exception if any.
-//      */
-//     public static void main(String[] args) throws Exception {
-//         // Create Pi4J console wrapper/helper
-//         // (This is a utility class to abstract some of the boilerplate stdin/stdout code)
-//         final var console = new Console();
+        System.out.println("... and now to the right in 2 more seconds!");
+        delay(Duration.ofSeconds(2));
+        servoMotor.setAngle(45);
 
-//         // Print program title/header
-//         console.title("<-- The Pi4J Project -->", "Minimal Example project");
+        // Use a custom range for displaying the data
+        System.out.println("Imagine a pointer on the servo positioned above a label between -20ºC and +40ºC");
+        System.out.println("By using the setRange() method, we can automatically map our temperature range to the servo range!");
+        System.out.println("As an example, in five seconds the servo will show -10º which should be on the far left of the servo.");
+        delay(Duration.ofSeconds(2));
 
-//         // ************************************************************
-//         //
-//         // WELCOME TO Pi4J:
-//         //
-//         // Here we will use this getting started example to
-//         // demonstrate the basic fundamentals of the Pi4J library.
-//         //
-//         // This example is to introduce you to the boilerplate
-//         // logic and concepts required for all applications using
-//         // the Pi4J library.  This example will do use some basic I/O.
-//         // Check the pi4j-examples project to learn about all the I/O
-//         // functions of Pi4J.
-//         //
-//         // ************************************************************
+        servoMotor.setRange(-20, +40); // This will define our range as values between -20 and +40
+        servoMotor.moveOnRange(-10); // This will map -10 based on the previously defined range
 
-//         // ------------------------------------------------------------
-//         // Initialize the Pi4J Runtime Context
-//         // ------------------------------------------------------------
-//         // Before you can use Pi4J you must initialize a new runtime
-//         // context.
-//         //
-//         // The 'Pi4J' static class includes a few helper context
-//         // creators for the most common use cases.  The 'newAutoContext()'
-//         // method will automatically load all available Pi4J
-//         // extensions found in the application's classpath which
-//         // may include 'Platforms' and 'I/O Providers'
-//         var pi4j = Pi4J.newAutoContext();
-
-//         // ------------------------------------------------------------
-//         // Output Pi4J Context information
-//         // ------------------------------------------------------------
-//         // The created Pi4J Context initializes platforms, providers
-//         // and the I/O registry. To help you to better understand this
-//         // approach, we print out the info of these. This can be removed
-//         // from your own application.
-//         // OPTIONAL
-//         PrintInfo.printLoadedPlatforms(console, pi4j);
-//         PrintInfo.printDefaultPlatform(console, pi4j);
-//         PrintInfo.printProviders(console, pi4j);
-
-//         // Here we will create I/O interfaces for a (GPIO) digital output
-//         // and input pin. We define the 'provider' to use PiGpio to control
-//         // the GPIO.
-//         var ledConfig = DigitalOutput.newConfigBuilder(pi4j)
-//                 .id("led")
-//                 .name("LED Flasher")
-//                 .address(PIN_LED)
-//                 .shutdown(DigitalState.LOW)
-//                 .initial(DigitalState.LOW)
-//                 .provider("pigpio-digital-output");
-//         var led = pi4j.create(ledConfig);
-
-//         var buttonConfig = DigitalInput.newConfigBuilder(pi4j)
-//                 .id("button")
-//                 .name("Press button")
-//                 .address(PIN_BUTTON)
-//                 .pull(PullResistance.PULL_DOWN)
-//                 .debounce(3000L)
-//                 .provider("pigpio-digital-input");
-//         var button = pi4j.create(buttonConfig);
-//         button.addListener(e -> {
-//             if (e.state() == DigitalState.LOW) {
-//                 pressCount++;
-//                 console.println("Button was pressed for the " + pressCount + "th time");
-//             }
-//         });
-
-//         // OPTIONAL: print the registry
-//         PrintInfo.printRegistry(console, pi4j);
-
-//         while (pressCount < 5) {
-//             if (led.equals(DigitalState.HIGH)) {
-//                 console.println("LED low");
-//                 led.low();
-//             } else {
-//                 console.println("LED high");
-//                 led.high();
-//             }
-//             Thread.sleep(500 / (pressCount + 1));
-//         }
-
-//         // ------------------------------------------------------------
-//         // Terminate the Pi4J library
-//         // ------------------------------------------------------------
-//         // We we are all done and want to exit our application, we must
-//         // call the 'shutdown()' function on the Pi4J static helper class.
-//         // This will ensure that all I/O instances are properly shutdown,
-//         // released by the the system and shutdown in the appropriate
-//         // manner. Terminate will also ensure that any background
-//         // threads/processes are cleanly shutdown and any used memory
-//         // is returned to the system.
-
-//         // Shutdown Pi4J
-//         pi4j.shutdown();
-//     }
-// }
- }
+        servoMotor.reset();
+    }
+    
+}
