27aac0d4d6d61733757a460eb93c7bcd6d558c08
Mary Livingston <marylivingston@Marys-MacBook-Pro.local>
2024-01-02 20:59:51 -0600

    added pi4j maven project

--- marepi4/src/main/java/com/marepi/Main.java
+++ marepi4/src/main/java/com/marepi/Main.java
@@ -1,6 +1,7 @@
+package com.marepi;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World! Hello Gentry!");
+        System.out.println("Hello world!");
     }
-    
 }
